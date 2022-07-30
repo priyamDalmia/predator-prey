bcbb29b4fec8aba2931558acc921b60a0ee7e6cb
Priyam Dalmia <priyam.dalmia200@gmail.com>
2022-07-30 17:19:34 +1000

    :abc: config for GPU support

--- data/config.py
+++ data/config.py
@@ -1,17 +1,37 @@
 from dataclasses import dataclass
+from typing import List, Tuple, Callable
 
-from typing import List
+@dataclass
+class TrainerConfig:
+    trainer: bool = False
+
+@dataclass
+class GameConfig:
+    env_name: str = "simplePP"
+    verbose: bool = False
+
+    npred: int = 10
+    nprey: int = 10    
+    pred_vision: int = 3
+    prey_vision: int = 3
+
+    time_mode: Tuple[bool, float] = (True, 500)
+    action_mode: int = None 
+    reward_mode: int = None
+
+@dataclass
+class ReplayBufferConfig:
+    pass
+
+@dataclass
+class GPUConfig:
+    pass
 
 @dataclass
 class Config:
-    env_name: str = "SimplePP"
-    
-    map_size: int = 6
-
-    npred = 2
-    nprey = 2 
-    
-    pred_vision = 3
-    prey_vision = 3
+    game_config: GameConfig = GameConfig()
+    trainer_config: TrainerConfig = TrainerConfig()
+    buffer_config: ReplayBufferConfig = ReplayBufferConfig()
+    gpu_config: GPUConfig = GPUConfig()
 
 
